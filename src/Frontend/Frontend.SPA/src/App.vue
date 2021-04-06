7596b02b6ac28b56113729d3ae972eb972b6c2ee
royappeldoorn <roy.appeldoorn@gmail.com>
2021-04-06 09:05:54 +0200

    Added correct vue project

---
+++ src/Frontend/Frontend.SPA/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="max-w-7xl mx-auto">
+    <div id="nav">
+      <Header />
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Header from '@/components/Header.vue';
+
+export default defineComponent({
+  components: {
+    Header
+  }
+});
+</script>
+
+<style lang="postcss">
+body {
+  @apply bg-gray-900 text-white;
+}
+
+#nav a {
+  font-weight: bold;
+  @apply text-gray-600;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/Frontend/Frontend.SPA/src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="home">
+    hoi
+  </div>
+</template>
+
+<script lang="ts"></script>
