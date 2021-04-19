1e529099b8c230c9ef8e14c783d1c89d88b6968b
royappeldoorn <roy.appeldoorn@gmail.com>
2021-04-19 16:28:33 +0200

    Added user service

--- src/Services/UserService/API/Program.cs
+++ src/Services/UserService/API/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace UserService
+namespace Kwetter.Services.UserService.API
 {
     public class Program
     {
--- src/Services/UserService/API/Startup.cs
+++ src/Services/UserService/API/Startup.cs
@@ -12,7 +12,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace UserService
+namespace Kwetter.Services.UserService.API
 {
     public class Startup
     {
@@ -30,7 +30,7 @@ namespace UserService
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "UserService", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
         }
 
@@ -41,7 +41,7 @@ namespace UserService
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "UserService v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
 
             app.UseHttpsRedirection();
