a4c6cf3f92427596e96e6ce5f3899666c407b0d2
royappeldoorn <roy.appeldoorn@gmail.com>
2021-05-10 15:29:04 +0200

    moved logic to domain model

--- src/Services/KweetService/API/Application/Commands/LikeKweet/LikeKweetCommandHandler.cs
+++ src/Services/KweetService/API/Application/Commands/LikeKweet/LikeKweetCommandHandler.cs
@@ -1,11 +1,8 @@
 ﻿using Kwetter.Services.Common.API.CQRS;
-using Kwetter.Services.KweetService.API.DataAccess;
 using Kwetter.Services.KweetService.API.DataAccess.Repositories;
-using Kwetter.Services.KweetService.API.Models;
 using MediatR;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -23,25 +20,15 @@ namespace Kwetter.Services.KweetService.API.Application.Commands.LikeKweet
         public async Task<CommandResult> Handle(LikeKweetCommand request, CancellationToken cancellationToken)
         {
             var kweet = await _kweetRepository.FindAsync(request.KweetId, cancellationToken);
-            bool liked = kweet.Likes.Any(x => x.UserId == request.UserId);
 
-            if (liked)
-            {
-                CommandResult error = new()
-                {
-                    Success = false,
-                    Errors = new List<string>() { "The kweet is already liked." }
-                };
-                return error;
-            }
-
-            // Add like to the kweet and update
-            kweet.AddLike(request.UserId);
-
+            bool liked = kweet.AddLike(request.UserId);
             bool success = await _kweetRepository.UnitOfWork.SaveEntitiesAsync(cancellationToken);
             CommandResult commandResult = new()
             {
-                Success = success,
+                Success = liked && success,
+                Errors = liked
+                    ? new List<string>()
+                    : new List<string>() { "The kweet is already liked." }
             };
             return commandResult;
         }
