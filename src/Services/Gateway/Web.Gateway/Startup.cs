eaf060f9d1465e6a87512f67c86ff1de2e1f9d14
royappeldoorn <roy.appeldoorn@gmail.com>
2021-05-13 19:18:34 +0200

    added firebase authentication

--- src/Services/Gateway/Web.Gateway/Startup.cs
+++ src/Services/Gateway/Web.Gateway/Startup.cs
@@ -1,8 +1,13 @@
+using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using System;
@@ -14,10 +19,44 @@ namespace Kwetter.Gateway.Web.Gateway
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //FirebaseApp firebaseApp = FirebaseApp.Create(new AppOptions()
+            //{
+            //    Credential = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") switch
+            //    {
+            //        "Production" => GoogleCredential.FromJson(_configuration["Authorization:Credential"]),
+            //        "Development" => GoogleCredential.FromFile(_configuration["Authorization:Credential"]),
+            //        _ => throw new ArgumentOutOfRangeException("Environment is not Production or Development."),
+            //    }
+            //});
+            //services.AddSingleton(firebaseApp);
+            string authenticationProviderKey = "firebase";
+
+            services
+               .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+               .AddJwtBearer(authenticationProviderKey, options =>
+               {
+                   options.Authority = "https://securetoken.google.com/kwetter-a60c1";
+                   options.TokenValidationParameters = new TokenValidationParameters
+                   {
+                       ValidateIssuer = true,
+                       ValidIssuer = "https://securetoken.google.com/kwetter-a60c1",
+                       ValidateAudience = true,
+                       ValidAudience = "kwetter-a60c1",
+                       ValidateLifetime = true
+                   };
+               });
+
             services.AddOcelot();
         }
 
@@ -30,13 +69,12 @@ namespace Kwetter.Gateway.Web.Gateway
             }
 
             app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
 
             app.UseOcelot().Wait();
