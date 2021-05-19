f01817b4bae15a2b740c9ec143ad3988c5f10988
royappeldoorn <roy.appeldoorn@gmail.com>
2021-05-19 15:50:46 +0200

    hide header on login/registration page

--- src/Frontend/Frontend.SPA/src/App.vue
+++ src/Frontend/Frontend.SPA/src/App.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class="mx-auto max-w-7xl">
-    <Header />
-    <div class="flex px-2 sm:px-6 lg:px-8">
-      <router-view />
+  <div id="app">
+    <div class="mx-auto max-w-7xl">
+      <Header v-if="authenticated" />
+      <div class="flex px-2 sm:px-6 lg:px-8">
+        <router-view />
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent } from 'vue';
 import Header from '@/components/Header.vue';
+import { rootStore } from './store';
+import { AuthGetterTypes } from './modules/auth/store/auth.getters';
+
 export default defineComponent({
   components: {
     Header,
+  },
+  setup() {
+    const authenticated = computed(() => rootStore.getters[AuthGetterTypes.GET_IS_LOGGED_IN]);
+
+    return {
+      authenticated,
+    };
   },
 });
 </script>
