f1b7c3d16cdd92ff482d138570a0fefea20aec2b
Roy <roy.appeldoorn@gmail.com>
2021-05-25 14:28:03 +0200

    form for sending a kweet

--- src/Frontend/Frontend.SPA/src/modules/home/index.vue
+++ src/Frontend/Frontend.SPA/src/modules/home/index.vue
@@ -1,19 +1,43 @@
 <script lang="ts">
 import { AuthGetterTypes } from '@/modules/auth/store/auth.getters';
 import { useStore } from '@/store';
-import { computed, defineComponent } from 'vue';
+import { computed, defineComponent, reactive, ref } from 'vue';
 
 export default defineComponent({
   name: 'Home',
   setup() {
     const store = useStore();
     const user = computed(() => store.getters[AuthGetterTypes.GET_USER]);
+    const kweet = ref('');
+    const loading = ref(false);
+    const input = reactive({ kweet });
+    const inputEmpty = computed(() => input.kweet === '');
+    return { user, loading, inputEmpty, postKweet };
 
-    return { user };
+    async function postKweet(): Promise<void> {
+      loading.value = true;
+    }
   },
 });
 </script>
 
 <template>
-  <div>{{ user }}</div>
+  <div class="flex flex-col">
+    <h1 class="text-xl font-bold dark:text-gray-100">Your feed</h1>
+    <form class="w-full" @submit.prevent="postKweet">
+      <div class="flex items-center border-b border-teal-500 py-2">
+        <input
+          v-model="input.kweet"
+          class="appearance-none bg-transparent border-none w-full text-gray-100 mr-3 py-1 px-2 leading-tight focus:outline-none"
+          type="text"
+          placeholder="Whats up?"
+          aria-label="Kweet"
+        />
+        <button class="flex-shrink-0 text-sm text-white py-2 px-4 rounded bg-red hover:bg-darkred" type="button">
+          Kweet
+        </button>
+      </div>
+    </form>
+    <div>{{ user }}</div>
+  </div>
 </template>
