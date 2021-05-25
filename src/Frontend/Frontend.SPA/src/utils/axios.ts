f8b8022472951fba2ec03d27d6f7ace652dc4ff7
Roy <roy.appeldoorn@gmail.com>
2021-05-25 13:08:49 +0200

    send idtoken with every request

--- src/Frontend/Frontend.SPA/src/utils/axios.ts
+++ src/Frontend/Frontend.SPA/src/utils/axios.ts
@@ -1,7 +1,16 @@
+import { auth } from '@/plugins/firebase';
 import axios, { AxiosInstance } from 'axios';
 
 const api: AxiosInstance = axios.create({
   baseURL: process.env.VUE_APP_GATEWAY_HOST,
+});
+
+api.interceptors.request.use(async function (config) {
+  const idToken = await auth.currentUser!.getIdToken();
+  config.headers.Authorization = idToken ? `Bearer ${idToken}` : '';
+  return config;
+}, function (error) {
+  return Promise.reject(error);
 });
 
 export default api;
