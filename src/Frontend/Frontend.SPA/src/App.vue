58037d2b912f967c9fc546cd4278b0afd35d19c8
Roy <roy.appeldoorn@gmail.com>
2021-05-25 13:14:31 +0200

    only render the router view

--- src/Frontend/Frontend.SPA/src/App.vue
+++ src/Frontend/Frontend.SPA/src/App.vue
@@ -1,33 +1,6 @@
 <template>
-  <div id="app">
-    <div class="mx-auto max-w-7xl">
-      <Header v-if="authenticated" />
-      <div class="flex px-2 sm:px-6 lg:px-8">
-        <router-view />
-      </div>
-    </div>
-  </div>
+  <router-view />
 </template>
-
-<script lang="ts">
-import { computed, defineComponent } from 'vue';
-import Header from '@/components/Header.vue';
-import { rootStore } from './store';
-import { AuthGetterTypes } from './modules/auth/store/auth.getters';
-
-export default defineComponent({
-  components: {
-    Header,
-  },
-  setup() {
-    const authenticated = computed(() => rootStore.getters[AuthGetterTypes.GET_IS_LOGGED_IN]);
-
-    return {
-      authenticated,
-    };
-  },
-});
-</script>
 
 <style lang="postcss">
 body {
