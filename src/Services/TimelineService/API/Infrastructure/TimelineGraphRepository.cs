3e553a2a181b1e9240de24e52aec8efe442181ff
royappeldoorn <roy.appeldoorn@gmail.com>
2021-06-08 16:16:59 +0200

    wrote final neo4j queries

--- src/Services/TimelineService/API/Infrastructure/TimelineGraphRepository.cs
+++ src/Services/TimelineService/API/Infrastructure/TimelineGraphRepository.cs
@@ -1,7 +1,9 @@
 ﻿using Kwetter.Services.TimelineService.API.Domain;
 using Neo4jClient;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace Kwetter.Services.TimelineService.API.Infrastructure
 {
@@ -29,7 +31,7 @@ namespace Kwetter.Services.TimelineService.API.Infrastructure
             await _client.Cypher
                 .Match("(user:User)")
                 .Where((User user) => user.Id == newKweet.UserId)
-                .Create("(kweet:Kweet {kweet})-[:KWEETED_BY]->(user)")
+                .Create("(kweet:Kweet $kweet)-[:KWEETED_BY]->(user)")
                 .WithParam("kweet", newKweet)
                 .ExecuteWithoutResultsAsync();
         }
@@ -47,9 +49,9 @@ namespace Kwetter.Services.TimelineService.API.Infrastructure
         public async Task CreateUserAsync(User user)
         {
             await _client.Cypher
-                .Merge("(user:User { Id: {id} })")
+                .Merge("(user:User { Id: $id })")
                 .OnCreate()
-                .Set("user = {user}")
+                .Set("user = $user")
                 .WithParams(new
                 {
                     id = user.Id,
@@ -58,19 +60,50 @@ namespace Kwetter.Services.TimelineService.API.Infrastructure
                 .ExecuteWithoutResultsAsync();
         }
 
-        public Task DeleteFollowerAsync(Guid followerId, Guid followingId)
+        public async Task DeleteFollowerAsync(Guid followerId, Guid followingId)
         {
-            throw new NotImplementedException();
+            await _client.Cypher
+                .Match("(following:User)-[follow:FOLLOWED_BY]->(follower:User)")
+                .Where((User following) => following.Id == followingId)
+                .AndWhere((User follower) => follower.Id == followerId)
+                .Delete("follow")
+                .ExecuteWithoutResultsAsync();
         }
 
-        public Task DeleteKweetLikeAsync(Guid userId, Guid kweetId)
+        public async Task DeleteKweetLikeAsync(Guid userId, Guid kweetId)
         {
-            throw new NotImplementedException();
+            await _client.Cypher
+                .Match("(user:User)-[like:LIKED_BY]->(kweet:Kweet)")
+                .Where((User user) => user.Id == userId)
+                .AndWhere((Kweet kweet) => kweet.Id == kweetId)
+                .Delete("like")
+                .ExecuteWithoutResultsAsync();
         }
 
-        public Task<Timeline> GetPaginatedTimelineAsync(Guid userId, uint pageNumber, uint pageSize)
+        public async Task<Timeline> GetPaginatedTimelineAsync(Guid userId, uint pageNumber, uint pageSize)
         {
-            throw new NotImplementedException();
+            Timeline timeline = new()
+            {
+                UserId = userId,
+                Kweets = new List<TimelineKweet>(),
+                PageNumber = pageNumber,
+                PageSize = pageSize
+            };
+
+            var query = _client.Cypher
+                .Match("(user:User)<-[:FOLLOWED_BY]-(user2:User)<-[:KWEETED_BY]-(kweet:Kweet)")
+                .Where((User user) => user.Id == userId)
+                .Skip((int)pageNumber * (int)pageSize)
+                .Limit((int)pageSize)
+                .Return((user, kweet) => new
+                {
+                    User = user.As<User>(),
+                    Kweets = kweet.CollectAs<Kweet>(),
+                });
+            var kek = (await query.ResultsAsync);
+            //timeline.Kweets = (await query.ResultsAsync).ToList();
+            
+            return timeline;
         }
     }
 }
